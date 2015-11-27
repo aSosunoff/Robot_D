471644419fe4429dcb2b6a99338761a7816b9b9b
aSosunoff <al.sosunoff@gmail.com>
2015-11-27 15:31:10 +0300

    Added small unit test for course method

---
+++ UnitTestHome/Test_Botom_Layer/TestCourse.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Robot_D.Bottom_Layer;
+using Robot_D.Exception;
+
+namespace TestPoint.Test_Botom_Layer
+{
+    [TestClass]
+    public class TestCourse
+    {
+        [TestMethod]
+        public void CourseTest()
+        {
+            var arr = new[]
+            {
+                "N", "n",
+                "E", "e",
+                "S", "s",
+                "W", "w",
+                "Q", "q"
+            };
+
+            foreach (var elDirection in arr)
+            {
+                try
+                {
+                    var course = new Course(elDirection);
+
+                    var actual = course.Direction;
+
+                    Assert.AreEqual(elDirection, actual);
+                }
+                catch (ExceptionUser w)
+                {
+                    Assert.AreEqual("Дрон может иметь только одно направление из (N, E, S, W)", w.Message);
+                }
+            }
+
+        }
+    }
+}
